9cb6a16ef4cbc95ac731e293b049fd4412b28548
didudua <126039750+didudua@users.noreply.github.com>
2025-02-14 09:43:33 +0700

    4.1

---
+++ 4_1_MaHoaCeasar.py
@@ -0,0 +1,17 @@
+# Cho k= STT , và Plaintext = TenCuaBan. Hãy mã hóa theo thuật toán Ceasar
+def Ceasar(k,Plaintext):
+    Ket_qua = ''
+    for char in Plaintext:
+        if char.isalpha():
+            if char.isupper():
+                Ket_qua += chr((ord(char) - ord('A') + k) % 26 + ord('A'))
+            elif char.islower(): 
+                Ket_qua += chr((ord(char) - ord('a') + k) % 26 + ord('a'))
+        else:
+            Ket_qua += char
+    return Ket_qua
+
+k = 19
+Plaintext = 'DoVanVu'
+
+print (Ceasar(k,Plaintext))
